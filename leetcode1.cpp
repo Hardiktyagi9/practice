0104a06798bd62f98360904df85f873b31d6d6a7
Hardik <tyagihardik4422@gmail.com>
2023-07-14 14:46:21 +0530

    added leetcode1

---
+++ leetcode1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+        string text = ["hello"] ;
+        int end1= text.size();
+        cout<< end1 <<endl;
+
+    return 0;
+}
